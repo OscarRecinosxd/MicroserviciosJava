790000e60ea8d240c7708aacbbc23f8169b76cce
OscarRecinosxd <oscarrecinos2019@gmail.com>
2023-10-19 09:14:17 -0600

    add project structure

--- payments/src/main/java/com/pamentsmicro/payments/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pamentsmicro.payments.repository;
-
-import com.pamentsmicro.payments.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- payments/src/test/java/com/pamentsmicro/payments/PaymentsApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package com.pamentsmicro.payments;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
-class PaymentsApplicationTest {
-
-}
