aff65a9651290aa1458d66376b31a016a15d79eb
OscarRecinosxd <oscarrecinos2019@gmail.com>
2023-10-28 01:27:50 -0600

    add docker compose file

--- paymentChairparent/businessdomain/product/src/main/java/com/oscarrecinos/product/controller/ProductController.java
+++ paymentChairparent/businessdomain/product/src/main/java/com/oscarrecinos/product/controller/ProductController.java
@@ -1,16 +1,13 @@
 package com.oscarrecinos.product.controller;
 
 import com.oscarrecinos.product.entity.Product;
-import com.oscarrecinos.product.repository.ProductRepository;
 import com.oscarrecinos.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/product")
@@ -18,12 +15,9 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @Value("${user.role}")
-    private String role;
 
     @GetMapping()
     public List<Product> findAll() {
-        System.out.println(role);
         return productService.getAll();
     }
 
